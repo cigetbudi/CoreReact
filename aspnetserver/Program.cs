50ed415e92ee4ccfd014166de140804f81bd6481
Sigit Budiarmawan <cigetbudi@outlook.com>
2022-03-24 16:37:40 +0700

    Creating the APIs

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -1,20 +1,87 @@
+using aspnetserver.Data;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ASP NET React Tut",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    swaggerUIOptions.DocumentTitle = "ASP NET React Tut";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API simple model");
+    swaggerUIOptions.RoutePrefix = String.Empty;
+});
+
 
 app.UseHttpsRedirection();
+
+//API Method
+app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync()).WithTags("Posts Endpoints");
+
+app.MapGet("/get-post-by-id/{postId}", async (int postId) => {
+    Post postToReturn = await PostsRepository.GetPostByIdAsync(postId);
+    if (postToReturn != null)
+    {
+        return Results.Ok(postToReturn);
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
+
+app.MapPost("/create-post", async (Post postToCreate) => {
+    bool isCreated = await PostsRepository.CreatePostAsync(postToCreate);
+    if (isCreated)
+    {
+        return Results.Ok("Berhasil ditembahkan");
+    }
+    else
+    {
+        return Results.BadRequest("Gagal ditambahkan");
+    }
+}).WithTags("Posts Endpoints");
+
+
+app.MapPut("/update-post", async (Post postToUpdate) => {
+    bool isUpdated = await PostsRepository.UpdatePostAsync(postToUpdate);
+    if (isUpdated)
+    {
+        return Results.Ok("Berhasil dirubah");
+    }
+    else
+    {
+        return Results.BadRequest("Gagal merubah");
+    }
+}).WithTags("Posts Endpoints");
+
+app.MapDelete("/delete-post-by-id", async (int postId) => {
+    bool isDeleted = await PostsRepository.DeletePostAsync(postId);
+    if (isDeleted)
+    {
+        return Results.Ok("Berhasil dihapus");
+    }
+    else
+    {
+        return Results.BadRequest("Gagal menghapus");
+    }
+}).WithTags("Posts Endpoints");
+
 
 
 app.Run();
