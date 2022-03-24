3439ec050f0184b03800944abdb66609976206d2
Sigit Budiarmawan <cigetbudi@outlook.com>
2022-03-24 12:58:18 +0700

    Database Context Setting

---
+++ aspnetserver/Data/AppDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspnetserver.Data
+{
+    internal sealed class AppDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source=./Data/AppDB.db"); 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Post[] postToSeed = new Post[6];
+            for (int i = 1; i <= 6; i++)
+            {
+                postToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    Title = $"Post {i}",
+                    Content = $"Ini adalah post ke {i}"
+                };
+            }
+            modelBuilder.Entity<Post>().HasData(postToSeed);
+        }
+    }
+}
---
+++ aspnetserver/Migrations/20220324055404_FirstMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetserver.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 100000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[] { 1, "Ini adalah post ke 1", "Post 1" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[] { 2, "Ini adalah post ke 2", "Post 2" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[] { 3, "Ini adalah post ke 3", "Post 3" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[] { 4, "Ini adalah post ke 4", "Post 4" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[] { 5, "Ini adalah post ke 5", "Post 5" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[] { 6, "Ini adalah post ke 6", "Post 6" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
